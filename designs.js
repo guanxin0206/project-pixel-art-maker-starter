78410467db0236f57790d87be2175f7e4ad11669
Xin Guan <xin.guan@unilever.com>
2025-04-01 01:40:49 +0800

    feat: complete the pixal art maker JavaScript files

--- designs.js
+++ designs.js
@@ -1,10 +1,38 @@
 // Select color input
 // Select size input
+const colorPicker = document.getElementById('colorPicker');
+const heightInput = document.getElementById('inputHeight');
+const widthInput = document.getElementById('inputWidth');
+const sizePicker = document.getElementById('sizePicker');
+const pixelCanvas = document.getElementById('pixelCanvas');
 
 // When size is submitted by the user, call makeGrid()
+sizePicker.addEventListener('submit', function (event) {
+  event.preventDefault();
 
-function makeGrid() {
+  const height = heightInput.value;
+  const width = widthInput.value;
 
-// Your code goes here!
+  // Call makeGrid function
+  makeGrid(width, height);
+});
 
+function makeGrid(width, height) {
+  // Clear existing grid
+  pixelCanvas.innerHTML = '';
+
+  for (let i = 0; i <= height; i++) {
+    const row = document.createElement('tr');
+    for (let j = 0; j <= width; j++) {
+      const cell = document.createElement('td');
+
+      // Add event listenr to change cell color
+      cell.addEventListener('click', function () {
+        cell.style.backgroundColor = colorPicker.value;
+      });
+
+      row.appendChild(cell);
+    }
+    pixelCanvas.appendChild(row);
+  }
 }
